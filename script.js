3e46f03cae71e6e1ed22c3311633447051bc27a8
Pradeep-003 <pradeep50953@gmail.com>
2025-07-25 08:00:00 +0530

    Enhance loader and cursor functionality; implement Locomotive Scroll integration and update styles

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+function locomotiveScrollTriggerCodePan() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
 function loadingAnimation() {
   var tl = gsap.timeline();
   tl.from(".line h1, .line h2", {
@@ -51,6 +91,13 @@ function loadingAnimation() {
     y: 120,
     stagger: 0.25,
   });
+  tl.from(
+    "#hero1, #page2",
+    {
+      opacity: 0,
+    },
+    "-=1.2"
+  );
 }
 
 function cursorAnimation() {
@@ -61,10 +108,11 @@ function cursorAnimation() {
     });
   });
 
-  
-
   Shery.makeMagnet("#nav-part2 h4");
 }
 
-loadingAnimation();
 cursorAnimation();
+loadingAnimation();
+locomotiveScrollTriggerCodePan();
+
+// Check console to see if it logs a LocomotiveScroll instance
